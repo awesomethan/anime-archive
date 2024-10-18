0003912390772be9a9d43955d6e4f113bc34e378
Ethan Wang <ethanwang1203@gmail.com>
2024-10-18 17:16:45 -0400

    fixed website title bug 2

---
+++ app/CustomHead.jsx
@@ -0,0 +1,11 @@
+import Head from "next/head";
+
+export default function CustomHead() {
+  return (
+    <div>
+      <Head>
+        <title>Ethan's Anime Archive</title>
+      </Head>
+    </div>
+  );
+}
