2a5d61e2c8b3abf68ed91cf2459e9f82bbc45fa3
Ethan Wang <ethanwang1203@gmail.com>
2024-10-22 18:41:04 -0400

    fixed webpage title; added loading screen for whole page if clerk component not done loading

--- app/layout.js
+++ app/layout.js
@@ -1,17 +1,7 @@
-// app/layout.js
-
 export const metadata = {
   title: {
-    absolute: "",
-    default: "Ethan's Anime Archive",
-    template: "",
+    default: "Ethan's Anime Archive", // Default title for the website
+    template: "%s | Ethan's Anime Archive", // Template for dynamic titles
   },
+  description: "An archive of anime curated by Ethan.", // Meta description
 };
-
-export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
-  );
-}
